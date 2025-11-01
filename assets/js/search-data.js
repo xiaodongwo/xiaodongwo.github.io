3fd92972e8952642593a4ece3327220096c55deb
xiaodongwo <73522191+xiaodongwo@users.noreply.github.com>
2025-11-01 15:09:23 +0000

    Deploying to gh-pages from @ xiaodongwo/xiaodongwo.github.io@ed3546ed74c2fff77d9c37fb1adfc4309e874068 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,7 +398,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-our-work-on-fluorescence-imaging-through-scattering-media-was-accepted-to-optics-and-lasers-in-engineering",
+            },},{id: "news-our-news-amp-amp-reviews-work-on-optical-coherence-tomography-was-published-in-light-science-amp-amp-applications",
+          title: 'Our news &amp;amp;amp; reviews work on Optical coherence tomography was published in Light:...',
+          description: "",
+          section: "News",},{id: "news-our-work-on-fluorescence-imaging-through-scattering-media-was-accepted-to-optics-and-lasers-in-engineering",
           title: 'Our work on Fluorescence Imaging through Scattering Media was accepted to Optics and...',
           description: "",
           section: "News",},{id: "news-our-work-on-super-resolution-fluorescence-imaging-was-accepted-to-chemical-communications",
